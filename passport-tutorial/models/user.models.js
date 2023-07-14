168f2f871c62357b04b2b76fb0b9d10a69ea740d
Ainal <ainal1haque@gmail.com>
2023-07-14 23:23:30 +0600

    password authentication normal

---
+++ passport-tutorial/models/user.models.js
@@ -0,0 +1,20 @@
+const mongoose =require("mongoose");
+
+
+const userSchema = new mongoose.Schema({
+    email: {
+      type: String,
+      reuire: true,
+    },
+    password: {
+      type: String,
+      reuire: true,
+    },
+    createdOn: {
+      type: Date,
+      default: Date.now,
+    },
+  });
+
+  
+  module.exports = mongoose.model("User", userSchema);
