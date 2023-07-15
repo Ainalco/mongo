51679df62428f5a55aa11175464b5e823274b16a
Ainal <ainal1haque@gmail.com>
2023-07-16 00:22:26 +0600

    password authentication normal part 3

--- passport-tutorial/app.js
+++ passport-tutorial/app.js
@@ -3,9 +3,10 @@ const cors=require("cors");
 const ejs=require("ejs");
 const User = require('./models/user.models');
 const app= express();
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 require("./config/database");
-require("./models/user.models")
-
+require("./models/user.models");
 app.set("view engine","ejs");
 app.use (cors());
 app.use(express.urlencoded({extended:true}));
@@ -28,9 +29,16 @@ app.post("/register", async (req,res)=>{
         if(user){
             return res.status(201).send("User is Already Exist");
         }else{
-            const newUser=User(req.body);
-            await newUser.save();
-            res.status(201).redirect("/login");
+            bcrypt.hash(req.body.password, saltRounds, async function(err, hash) {
+                const newUser = new User({
+                    username: req.body.username,
+                  password:hash,
+                });
+                
+                await newUser.save();
+                res.status(201).redirect("/login");
+            });
+            
         }
     } catch (error) {
         res.status(500).send(error.message);
